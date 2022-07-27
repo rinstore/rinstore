7bfc3db37dbd6dba38b798c6316698ed0952fc4c
honeydev <lebedev.alexey@protonmail.com>
2022-07-28 01:01:25 +1000

    Add registration page styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :user_name => "devspadesdev",
+    :password => "ysjx5fnc",
+    :port => 587,
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
